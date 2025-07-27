ac253cfa387a35a049a331ff591100377920a259
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-07-27 19:20:09 +0100

    Check that the data is paired properly

--- calibration_data/averages_from_log.py
+++ calibration_data/averages_from_log.py
@@ -14,16 +14,35 @@ def print_averages_from_log(log_data: str):
     sensor_1_data: list[list[float]] = []
     sensor_2_data: list[list[float]] = []
     reading_index = -1
+    previous_seconds = -1
     for line in log_data.splitlines():
-        regex = r"\[..:..:..]\[D]\[bmi160:243]: Got accel={x=([\-\.\d]+) m\/s², y=([\-\.\d]+) m\/s², z=([\-\.\d]+) m\/s²}, gyro={x=([\-\.\d]+) °\/s, y=([\-\.\d]+) °\/s, z=([\-\.\d]+) °\/s}. temp=([\-\.\d]+)°C"
+        regex = r"\[..:..:(\d\d)]\[D]\[bmi160:243]: Got accel={x=([\-\.\d]+) m\/s², y=([\-\.\d]+) m\/s², z=([\-\.\d]+) m\/s²}, gyro={x=([\-\.\d]+) °\/s, y=([\-\.\d]+) °\/s, z=([\-\.\d]+) °\/s}. temp=([\-\.\d]+)°C"
         result = re.search(regex, line)
         if not result:
             continue
         reading_index += 1
         values = [float(value) for value in result.groups()]
-        if reading_index % 2:
+        seconds = int(values.pop(0))
+        if reading_index % 2 == 0:
+            if previous_seconds == seconds:
+                print(line)
+                print(
+                    f"Previous seconds={previous_seconds}, expected a change, got {seconds}"
+                )
+                raise ValueError(
+                    f"Inconsistency in the data @{reading_index} (started at line printed above)"
+                )
+            previous_seconds = seconds
             sensor_1_data.append(values)
         else:
+            if previous_seconds != seconds:
+                print(line)
+                print(
+                    f"Previous seconds={previous_seconds}, expected the same, got {seconds}"
+                )
+                raise ValueError(
+                    f"Inconsistency in the data @{reading_index} (started at line printed above)"
+                )
             sensor_2_data.append(values)
 
     sensor_1_averages = [fmean(values) for values in zip(*sensor_1_data)]
