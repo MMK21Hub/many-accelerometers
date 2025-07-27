819fd589491e4983158e62c6171e194ba18a4419
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-07-27 19:36:08 +0100

    Make the pairing check a warning

--- calibration_data/averages_from_log.py
+++ calibration_data/averages_from_log.py
@@ -15,7 +15,9 @@ def print_averages_from_log(log_data: str):
     sensor_2_data: list[list[float]] = []
     reading_index = -1
     previous_seconds = -1
-    for line in log_data.splitlines():
+    lines = log_data.split("\n")
+    for line_index in range(len(lines)):
+        line = lines[line_index]
         regex = r"\[..:..:(\d\d)]\[D]\[bmi160:243]: Got accel={x=([\-\.\d]+) m\/s², y=([\-\.\d]+) m\/s², z=([\-\.\d]+) m\/s²}, gyro={x=([\-\.\d]+) °\/s, y=([\-\.\d]+) °\/s, z=([\-\.\d]+) °\/s}. temp=([\-\.\d]+)°C"
         result = re.search(regex, line)
         if not result:
@@ -23,26 +25,25 @@ def print_averages_from_log(log_data: str):
         reading_index += 1
         values = [float(value) for value in result.groups()]
         seconds = int(values.pop(0))
+        # We assume every even reading is from sensor 1 and every odd reading is from sensor 2
         if reading_index % 2 == 0:
             if previous_seconds == seconds:
-                print(line)
+                # We expect exactly two sensor readings (one from each sensor) each second.
+                # If that doesn't happen, then it might mean that the order of the sensors gets swapped around, which would ruin our data.
+                print(f"Warning: Found questionable pair of readings @{line_index + 1}")
                 print(
-                    f"Previous seconds={previous_seconds}, expected a change, got {seconds}"
+                    f"  Previous reading seconds={previous_seconds}, expected an increment, got {seconds}"
                 )
-                raise ValueError(
-                    f"Inconsistency in the data @{reading_index} (started at line printed above)"
-                )
+                print(f"  {line}")
             previous_seconds = seconds
             sensor_1_data.append(values)
         else:
             if previous_seconds != seconds:
-                print(line)
+                print(f"Warning: Found questionable pair of readings @{line_index + 1}")
                 print(
-                    f"Previous seconds={previous_seconds}, expected the same, got {seconds}"
+                    f"  Previous reading seconds={previous_seconds}, expected it to stay the same, got {seconds}"
                 )
-                raise ValueError(
-                    f"Inconsistency in the data @{reading_index} (started at line printed above)"
-                )
+                print(f"  {line}")
             sensor_2_data.append(values)
 
     sensor_1_averages = [fmean(values) for values in zip(*sensor_1_data)]
